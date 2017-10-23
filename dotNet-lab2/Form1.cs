5ecb774784de22f2b37d6b7c0920c5222e31f177
MamakovAV <MamakovAV@W12MAMAKOVAV>
2017-10-23 15:49:13 +0300

    lab2 done

--- dotNet-lab2/Form1.cs
+++ dotNet-lab2/Form1.cs
@@ -16,5 +16,19 @@ namespace dotNet_lab2
         {
             InitializeComponent();
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DialogResult aResult;
+            Form2 aForm = new Form2();
+            aResult = aForm.ShowDialog();
+            if (aResult == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show("Your name is " + aForm.textBox1.Text + " " +
+                aForm.textBox2.Text);
+            }
+            linkLabel1.LinkVisited = true;
+
+        }
     }
 }
